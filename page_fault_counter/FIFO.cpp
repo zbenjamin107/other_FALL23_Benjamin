9bad61a54f1bcdfbc0b4e814b3fe5e4d990a1d20
Zachary L. Benjamin <98478363+zbenjamin107@users.noreply.github.com>
2023-11-09 17:03:01 -0500

    Create FIFO.cpp

---
+++ page_fault_counter/FIFO.cpp
@@ -0,0 +1,29 @@
+#include "FIFO.h"
+
+FIFO::FIFO(int numPageFrames) : ReplacementAlgorithm(numPageFrames) {
+    pageFaultCount = 0;
+    pageFrames = numPageFrames;
+}
+
+void FIFO::insert(int pageNumber)   
+{
+    // Implement FIFO page replacement algorithm 
+    // Increment pageFaultCount if a page fault occurs
+
+    fault = true;       //assume there will be a fault
+    for (int i : list) {//loops through the pages and test to see if it is on the list already 
+        if (i == pageNumber) {
+            fault = false;  //if we do find a match we will set page fault to false
+        }
+    }
+
+    if (fault) {//if there is a page fault add to page faults and push the new page
+        ++pageFaultCount;
+        list.push_front(pageNumber);
+        if (list.size() == pageFrames + 1) list.pop_back();//this makes sure we add to list until it is size 4
+    }
+
+    //else..nothing. we dont arrange the order. we dont add
+
+}
+
