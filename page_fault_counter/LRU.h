6ae8f13893e76df15db5225bd25bf400b1ae9422
Zachary L. Benjamin <98478363+zbenjamin107@users.noreply.github.com>
2023-11-09 16:51:00 -0500

    Create LRU.h

---
+++ page_fault_counter/LRU.h
@@ -0,0 +1,22 @@
+#ifndef _LRU_H
+#define _LRU_H
+
+#include <iostream>
+#include "ReplacementAlgorithm.h"
+#include <list>
+
+class LRU : public ReplacementAlgorithm {
+public:
+    LRU(int numPageFrames);
+    void insert(int pageNumber) override;
+    
+
+private:
+    // data structure to store the int page frame list
+    //list of int 
+    int pageFrames;
+    std::list<int> list;
+    bool fault;
+};
+
+#endif
